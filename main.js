32868bd35ef990a6c8376a6ee7dec8c354bf9fc8
Matt Westwick <matt.westwick@gmail.com>
2021-01-03 19:22:46 -0600

    tidied up

--- main.js
+++ main.js
@@ -10,15 +10,58 @@ const MapUploadToKeys = {
   box4:  "Bottle 4 box",
   checkedIn: "Checked In"
 }
+const boxes = ['box1', 'box2', 'box3', 'box4']
 const Entries = {}
-const BCOEM = {
-  url: '',
-  password: ''
+let bcoemConnected = false;
+
+const connectToBcoem = (url, password) => {
+  const form = new FormData()
+  form.append("inputPassword", password)
+
+  return $.ajax({
+    url: `${url.replace(/\/+$/, "")}/qr.php?action=password-check`,
+    method: "POST",
+    data: form,
+    processData: false,
+    contentType: false,
+  })
+  .then(response => {
+    if (response.includes("Password accepted.")) {
+      return Promise.resolve("Connected")
+    } else if (response.includes("Password incorrect.")) {
+      return Promise.reject("Incorrect password")
+    }
+
+    return Promise.reject("Connection Failure")
+  })
+}
+
+const checkInEntry = (url, entryNumber) => {
+  return $.ajax(
+  {
+    url: `${url.replace(/\/+$/, "")}/qr.php?action=update&id=${Number(entryNumber)}`,
+    method: "POST",
+    xhrFields: {
+      withCredentials: true
+    }
+  })
+  .then(response => {
+    if (response.includes("is checked in.</strong></p>")) {
+      return Promise.resolve(entryNumber)
+    } else if (response.includes("please provide the correct password.")) {
+      return Promise.reject("Session Timed Out")
+    }
+
+    return Promise.reject("Checkin Failure")
+  })
 }
 
 $(document).ready(() => {
+  $('#scannerInput').focus()
+
   const table = $("#entry-table").DataTable({
-    columns: Object.keys(MapUploadToKeys).map(key => ({data: key}))
+    columns: Object.keys(MapUploadToKeys).map(key => ({"data": key})),
+    rowId: "entryNumber"
   })
 
   $('#scannerInput').keyup((evt) => {
@@ -33,17 +76,42 @@ $(document).ready(() => {
       return
     }
 
-    const printHtml = `<div><div style="font-weight:bolder;"># ${scannedEntry.judgingNumber}</div><div>Cat: ${scannedEntry.category}${scannedEntry.subcat} &nbsp; Box: ${scannedEntry.box1}</div><div>Flight: ${scannedEntry.flight}</div></div>`
     const printCss="margin:0px;width:1in; height:1in; transform:rotate(90deg);display:flex;flex-direction:column;justify-content:space-around;align-items:center;font-size:9.5px;font-family:sans-serif;"
 
-    const printWindow = window.open('', 'PRINT', 'height=400,width=600')
-    printWindow.document.write(`<html><head><title>Print Label</title></head><body style="${printCss}">${printHtml}${printHtml}</body></html>`)
-    printWindow.document.close()
-    printWindow.focus()
-    printWindow.print()
-    printWindow.close()
+    boxes.forEach(boxId => {
+      if (scannedEntry[boxId] === null || scannedEntry[boxId] === undefined) return
 
-    console.log(scannedEntry)
+      const printHtml = `<div><div style="font-weight:bolder;"># ${scannedEntry.judgingNumber}</div><div>Cat: ${scannedEntry.category}${scannedEntry.subcat} &nbsp; Box: ${scannedEntry[boxId]}</div><div>Flight: ${scannedEntry.flight}</div></div>`
+    
+      const printWindow = window.open('', 'PRINT', 'height=400,width=600')
+      printWindow.document.write(`<html><head><title>Print Label</title></head><body style="${printCss}">${printHtml}${printHtml}</body></html>`)
+      printWindow.document.close()
+      printWindow.focus()
+      // printWindow.print()
+      printWindow.close()
+    })
+
+    const url = $('#competitionUrl').val()
+
+    checkInEntry(url, scannedEntry.entryNumber)
+    .then(entryNumber => {
+      $('#checkInStatus').attr('hidden', false)
+      $('#checkInStatus').removeClass("alert-warning alert-danger alert-primary")
+      $('#checkInStatus').addClass("alert-success")
+      $('#checkInStatus').text(`Entry ${entryNumber} checked in`)
+
+      table.cell(`#${entryNumber}`, 9).data("✓").draw()
+    })
+    .catch(err => {
+      $('#checkInStatus').attr('hidden', false)
+      $('#checkInStatus').removeClass("alert-warning alert-success alert-primary")
+      $('#checkInStatus').addClass("alert-danger")
+      $('#checkInStatus').text(`Error adding entry ${scannedEntry.entryNumber}: ${err}`)
+
+      table.cell(`#${entryNumber}`, 9).data("✗").draw()
+    })
+
+    $('#scannerInput').focus()
   })
 
   $('#entryFileUpload').on('change', handleFile)
@@ -54,6 +122,32 @@ $(document).ready(() => {
 
   $("#addEntryButton").on("click", ()=> {
     $('#addEntryModal').modal('show')
+  })
+
+  $('#BcoemConnectButton').on("click", () => {
+    const url = $('#competitionUrl').val()
+    const password = $('#qr-checkin-password').val()
+
+    if (bcoemConnected === true) {
+      bcoemConnected = false
+      updateConnectionUi()
+      $('#connectionStatus').text("Disconnected")
+      return
+    }
+
+    if (url === "" || password === "") return
+
+    connectToBcoem(url, password)
+    .then(() => {
+      bcoemConnected = true;
+      updateConnectionUi()
+    })
+    .catch(err => {
+      bcoemConnected = false;
+      updateConnectionUi()
+      $('#connectionStatus').text("Error: " + err)
+      
+    })
   })
 
   $('#addManualEntry').on("click", () => {
@@ -81,6 +175,27 @@ $(document).ready(() => {
     $("#addBox3").val('')
     $("#addBox4").val('')
   })
+
+  function updateConnectionUi() {
+    if (bcoemConnected) {
+      $('#connectionStatus').removeClass("alert-warning alert-danger alert-primary")
+      $('#connectionStatus').addClass("alert-success")
+      $('#connectionStatus').text("✓ Connected")
+      $('#qr-checkin-password').attr("disabled", true)
+      $('#competitionUrl').attr("disabled", true)
+      $('#BcoemConnectButton').removeClass("btn-primary")
+      $('#BcoemConnectButton').addClass("btn-danger")
+      $('#BcoemConnectButton').text("Disconnect")
+    } else {
+      $('#connectionStatus').removeClass("alert-warning alert-success alert-primary")
+      $('#connectionStatus').addClass("alert-danger")
+      $('#qr-checkin-password').attr("disabled", false)
+      $('#competitionUrl').attr("disabled", false)
+      $('#BcoemConnectButton').removeClass("btn-danger")
+      $('#BcoemConnectButton').addClass("btn-primary")
+      $('#BcoemConnectButton').text("Connect")
+    }
+  }
 
   function handleFile(e) {
     const files = e.target.files, f = files[0];
