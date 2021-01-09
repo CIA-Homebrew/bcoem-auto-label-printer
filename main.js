6044330adf6642bac156a3467a6f1daa973b8c89
Matt Westwick <matt.westwick@gmail.com>
2021-01-09 01:28:39 -0600

    debugging

--- main.js
+++ main.js
@@ -25,7 +25,9 @@ const connectToBcoem = (url, password) => {
     processData: false,
     contentType: false,
   })
-  .then(response => {
+  .then((response, status, xhr) => {
+    console.log(xhr, xhr.getResponseHeader("set-cookie"))
+
     if (response.includes("Password accepted.")) {
       return Promise.resolve("Connected")
     } else if (response.includes("Password incorrect.")) {
