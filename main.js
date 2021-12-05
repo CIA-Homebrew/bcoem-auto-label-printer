149ca756c97c451ea7b28611df6826a691fb84a1
Matt Westwick <matt.westwick@gmail.com>
2021-12-04 23:17:09 -0600

    added legacy labels for macos

--- main.js
+++ main.js
@@ -81,13 +81,12 @@ const printDefault = (scannedEntry) => {
 
 const printDymo = (scannedEntry) => {
   boxes.forEach(boxId => {
-    // const generatedXML = jQuery.parseXML(generateXML(scannedEntry, boxId))
-    const generatedXML = generateXML(scannedEntry, boxId)
+    const generatedXML = generateLegacyXML(scannedEntry, boxId)
 
     label = dymo.label.framework.openLabelXml(generatedXML)
+    const isValidLabel = label.isValidLabel()
 
-    if (dymoPrinter) {
-      console.log(generatedXML)
+    if (dymoPrinter && isValidLabel) {
       label.print(dymoPrinter)
     }
   })
@@ -710,3 +709,171 @@ const generateXML = (scannedEntry, boxId) => `<?xml version="1.0" encoding="utf-
   </DataTable>
 </DesktopLabel>
 `
+
+
+const generateLegacyXML = (scannedEntry, boxId) => `<?xml version="1.0" encoding="utf-8"?>
+  <DieCutLabel Version="8.0" Units="twips">
+    <PaperOrientation>Landscape</PaperOrientation>
+    <Id>Address</Id>
+    <PaperName>30252 Address</PaperName>\
+    <DrawCommands>
+        <RoundRectangle X="0" Y="0" Width="1440" Height="1440" Rx="270" Ry="270" />
+    </DrawCommands>
+
+    <ObjectInfo>
+      <TextObject>
+        <Name>Top Text</Name>
+        <ForeColor Alpha="255" Red="0" Green="0" Blue="0" />
+        <BackColor Alpha="0" Red="255" Green="255" Blue="255" />
+        <LinkedObjectName></LinkedObjectName>
+        <Rotation>Rotation0</Rotation>
+        <IsMirrored>False</IsMirrored>
+        <IsVariable>False</IsVariable>
+        <HorizontalAlignment>Left</HorizontalAlignment>
+        <VerticalAlignment>Top</VerticalAlignment>
+        <TextFitMode>ShrinkToFit</TextFitMode>
+        <UseFullFontHeight>True</UseFullFontHeight>
+        <Verticalized>False</Verticalized>
+        <StyledText>
+          <Element>
+            <String># ${scannedEntry.judgingNumber}</String>
+            <Attributes>
+              <Font Family="Arial" Size="10" Bold="True" Italic="False" Underline="False" Strikeout="False" />
+              <ForeColor Alpha="255" Red="0" Green="0" Blue="0" />
+            </Attributes>
+          </Element>
+        </StyledText>
+      </TextObject>
+      <Bounds X="144" Y="120" Width="1440" Height="300" />
+    </ObjectInfo>
+    <ObjectInfo>
+      <TextObject>
+        <Name>Top Text</Name>
+        <ForeColor Alpha="255" Red="0" Green="0" Blue="0" />
+        <BackColor Alpha="0" Red="255" Green="255" Blue="255" />
+        <LinkedObjectName></LinkedObjectName>
+        <Rotation>Rotation0</Rotation>
+        <IsMirrored>False</IsMirrored>
+        <IsVariable>False</IsVariable>
+        <HorizontalAlignment>Left</HorizontalAlignment>
+        <VerticalAlignment>Top</VerticalAlignment>
+        <TextFitMode>ShrinkToFit</TextFitMode>
+        <UseFullFontHeight>True</UseFullFontHeight>
+        <Verticalized>False</Verticalized>
+        <StyledText>
+          <Element>
+            <String>${scannedEntry[boxId] && boxId !== "records" ? "Box:" : ""}${scannedEntry[boxId] || ""} Cat:${scannedEntry.category}${scannedEntry.subcat}</String>
+            <Attributes>
+              <Font Family="Arial" Size="8" Bold="False" Italic="False" Underline="False" Strikeout="False" />
+              <ForeColor Alpha="255" Red="0" Green="0" Blue="0" />
+            </Attributes>
+          </Element>
+        </StyledText>
+      </TextObject>
+      <Bounds X="144" Y="400" Width="1440" Height="300" />
+    </ObjectInfo>
+    <ObjectInfo>
+      <TextObject>
+        <Name>Top Text</Name>
+        <ForeColor Alpha="255" Red="0" Green="0" Blue="0" />
+        <BackColor Alpha="0" Red="255" Green="255" Blue="255" />
+        <LinkedObjectName></LinkedObjectName>
+        <Rotation>Rotation0</Rotation>
+        <IsMirrored>False</IsMirrored>
+        <IsVariable>False</IsVariable>
+        <HorizontalAlignment>Left</HorizontalAlignment>
+        <VerticalAlignment>Top</VerticalAlignment>
+        <TextFitMode>ShrinkToFit</TextFitMode>
+        <UseFullFontHeight>True</UseFullFontHeight>
+        <Verticalized>False</Verticalized>
+        <StyledText>
+          <Element>
+            <String>${boxId === 'record' ? "FOR RECORDS" : (scannedEntry[boxId] === null || scannedEntry[boxId] === undefined) ? "SPARE ENTRY" : boxId === 'box3' ? "Flight: 2nd Round" : boxId === 'box4' ? "Flight: BOS" : "FlightID: " + scannedEntry.flight}</String>
+            <Attributes>
+              <Font Family="Arial" Size="8" Bold="False" Italic="False" Underline="False" Strikeout="False" />
+              <ForeColor Alpha="255" Red="0" Green="0" Blue="0" />
+            </Attributes>
+          </Element>
+        </StyledText>
+      </TextObject>
+      <Bounds X="144" Y="588" Width="1440" Height="300" />
+    </ObjectInfo>
+    <ObjectInfo>
+      <TextObject>
+        <Name>Top Text</Name>
+        <ForeColor Alpha="255" Red="0" Green="0" Blue="0" />
+        <BackColor Alpha="0" Red="255" Green="255" Blue="255" />
+        <LinkedObjectName></LinkedObjectName>
+        <Rotation>Rotation0</Rotation>
+        <IsMirrored>False</IsMirrored>
+        <IsVariable>False</IsVariable>
+        <HorizontalAlignment>Left</HorizontalAlignment>
+        <VerticalAlignment>Top</VerticalAlignment>
+        <TextFitMode>ShrinkToFit</TextFitMode>
+        <UseFullFontHeight>True</UseFullFontHeight>
+        <Verticalized>False</Verticalized>
+        <StyledText>
+          <Element>
+            <String># ${scannedEntry.judgingNumber}</String>
+            <Attributes>
+              <Font Family="Arial" Size="10" Bold="True" Italic="False" Underline="False" Strikeout="False" />
+              <ForeColor Alpha="255" Red="0" Green="0" Blue="0" />
+            </Attributes>
+          </Element>
+        </StyledText>
+      </TextObject>
+      <Bounds X="144" Y="840" Width="1440" Height="300" />
+    </ObjectInfo>
+    <ObjectInfo>
+      <TextObject>
+        <Name>Top Text</Name>
+        <ForeColor Alpha="255" Red="0" Green="0" Blue="0" />
+        <BackColor Alpha="0" Red="255" Green="255" Blue="255" />
+        <LinkedObjectName></LinkedObjectName>
+        <Rotation>Rotation0</Rotation>
+        <IsMirrored>False</IsMirrored>
+        <IsVariable>False</IsVariable>
+        <HorizontalAlignment>Left</HorizontalAlignment>
+        <VerticalAlignment>Top</VerticalAlignment>
+        <TextFitMode>ShrinkToFit</TextFitMode>
+        <UseFullFontHeight>True</UseFullFontHeight>
+        <Verticalized>False</Verticalized>
+        <StyledText>
+          <Element>
+            <String>${scannedEntry[boxId] && boxId !== "records" ? "Box:" : ""}${scannedEntry[boxId] || ""} Cat:${scannedEntry.category}${scannedEntry.subcat}</String>
+            <Attributes>
+              <Font Family="Arial" Size="8" Bold="False" Italic="False" Underline="False" Strikeout="False" />
+              <ForeColor Alpha="255" Red="0" Green="0" Blue="0" />
+            </Attributes>
+          </Element>
+        </StyledText>
+      </TextObject>
+      <Bounds X="144" Y="1120" Width="1440" Height="300" />
+    </ObjectInfo>
+    <ObjectInfo>
+      <TextObject>
+        <Name>Top Text</Name>
+        <ForeColor Alpha="255" Red="0" Green="0" Blue="0" />
+        <BackColor Alpha="0" Red="255" Green="255" Blue="255" />
+        <LinkedObjectName></LinkedObjectName>
+        <Rotation>Rotation0</Rotation>
+        <IsMirrored>False</IsMirrored>
+        <IsVariable>False</IsVariable>
+        <HorizontalAlignment>Left</HorizontalAlignment>
+        <VerticalAlignment>Top</VerticalAlignment>
+        <TextFitMode>ShrinkToFit</TextFitMode>
+        <UseFullFontHeight>True</UseFullFontHeight>
+        <Verticalized>False</Verticalized>
+        <StyledText>
+          <Element>
+            <String>${boxId === 'record' ? "FOR RECORDS" : (scannedEntry[boxId] === null || scannedEntry[boxId] === undefined) ? "SPARE ENTRY" : boxId === 'box3' ? "Flight: 2nd Round" : boxId === 'box4' ? "Flight: BOS" : "FlightID: " + scannedEntry.flight}</String>
+            <Attributes>
+              <Font Family="Arial" Size="8" Bold="False" Italic="False" Underline="False" Strikeout="False" />
+              <ForeColor Alpha="255" Red="0" Green="0" Blue="0" />
+            </Attributes>
+          </Element>
+        </StyledText>
+      </TextObject>
+      <Bounds X="144" Y="1308" Width="1440" Height="120" />
+    </ObjectInfo>
+  </DieCutLabel>`
